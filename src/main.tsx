e4780ae96807fc0b9cb4967a7789e4e99fc7202c
Thorlani <tolanipopoola07@gmail.com>
2023-04-30 23:15:22 +0100

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { inject } from "@vercel/analytics";
+
+inject();
 
 const queryClient = new QueryClient();
 
